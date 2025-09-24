9b1be2dcbeb65e2e2ffffca2b58061e00cbf8c3b
Ruslan_Trafimovich <ruworv@gmail.com>
2025-09-24 17:22:52 +0300

    test

---
+++ src/controllers/usersController.js
@@ -0,0 +1,15 @@
+
+class userController {
+
+    async createUser(req, res) {
+        try {
+            
+
+        } catch (err) {
+
+        }
+    }
+
+};
+
+module.exports = new userController();
