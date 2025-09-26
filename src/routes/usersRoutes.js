c5cef63ad3a07408cac655826362297aafc42759
Ruslan_Trafimovich <rusworv@gmail.com>
2025-09-26 14:47:43 +0300

    JWT Done

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const UsersControllers = require('../controllers/usersControllers')
+const authorizeToken = require('../middleware/authorizeToken')
 
 router.post('/register', UsersControllers.createUser);
-router.get('/', UsersControllers.getUsers);
+router.get('/', authorizeToken, UsersControllers.getUsers);
 router.post('/login', UsersControllers.loginUser);
 
 
