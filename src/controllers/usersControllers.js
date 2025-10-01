53f02b1398ccd3888bcf44e5fd723cb0284631d0
Ruslan_Trafimovich <rusworv@gmail.com>
2025-10-01 23:14:49 +0300

    test test test

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,6 +1,6 @@
 //usersControllers.js
 const usersServices = require("../services/usersServices");
-const {ValidationError} = require('../errors/customErrors')
+const { ValidationError } = require("../errors/customErrors");
 
 class UsersControllers {
   async createUser(req, res, next) {
@@ -10,7 +10,10 @@ class UsersControllers {
         throw new ValidationError("Email и пароль обязательны");
       }
       const cleanEmail = email.trim().toLowerCase();
-      const createdUser = await usersServices.createUser({email: cleanEmail, password});
+      const createdUser = await usersServices.createUser({
+        email: cleanEmail,
+        password,
+      });
       res.status(201).json({
         success: true,
         message: "Пользователь успешно зарегистрирован",
@@ -21,26 +24,43 @@ class UsersControllers {
     }
   }
 
-  async getUsers(req, res, next) {
+  async loginUser(req, res, next) {
     try {
-      const result = await usersServices.getUsers();
-      res.send(result);
+      const { email, password } = req.body;
+      const tokenAndUser = await usersServices.authenticateUser(
+        email,
+        password
+      );
+      const { token, user } = tokenAndUser;
+      res.json({
+        success: true,
+        message: "Успешный вход в систему",
+        data: { token, user },
+      });
     } catch (err) {
       next(err);
     }
   }
 
-  async loginUser(req, res, next) {
+  async getUsers(req, res, next) {
     try {
-      const { email, password } = req.body;
-      const tokenAndUser = await usersServices.authenticateUser(email, password);
-      const {token, user} = tokenAndUser
+      const users = await usersServices.getUsers();
       res.json({
         success: true,
-        message: 'Успешный вход в систему',
-        data: { token, user }
+        data: { users },
       });
-  
+    } catch (err) {
+      
+    }
+  }
+
+  async getCurrentUser(req, res, next) {
+    try {
+      const user = await usersServices.getUserById(req.userId);
+      res.json({
+        success: true,
+        data: { user },
+      });
     } catch (err) {
         next(err);
     }
--- src/services/usersServices.js
+++ src/services/usersServices.js
@@ -3,55 +3,62 @@ const { models } = require("../models/index");
 const { User } = models;
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const {ConflictError, AuthenticationError} = require('../errors/customErrors')
-
+const {
+  ConflictError,
+  AuthenticationError,
+} = require("../errors/customErrors");
 
 class UsersServices {
   async createUser(userData) {
-    const {email, password} = userData;
-        // Проверка существования пользователя
-        const existingUser = await User.findOne({ where: { email } });
-        if (existingUser) {
-          throw new ConflictError('Пользователь с таким email уже существует');
-        };
-        const user = await User.create({email, password})
+    const { email, password } = userData;
+    // Проверка существования пользователя
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      throw new ConflictError("Пользователь с таким email уже существует");
+    }
+    const user = await User.create({ email, password });
     return user;
-  };
-
-  async getUsers() {
-    const users = await User.findAll({});
-    return users;
-  };
-
-  async getUserById(id) {
-    const user = await User.findByPk(id)
-    return user;
-  };
+  }
 
   async authenticateUser(email, password) {
     const user = await User.findOne({
-      where: { email: email }
+      where: { email: email },
     });
-    
+
     if (!user) {
-      throw new AuthenticationError ('Неверный email или пароль');
+      throw new AuthenticationError("Неверный email или пароль");
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      throw new AuthenticationError ('Неверный email или пароль');
-    };
+      throw new AuthenticationError("Неверный email или пароль");
+    }
 
     const token = jwt.sign(
-      { userId: user.id.toString(),
-        email: user.email
-       },
+      { userId: user.id.toString(), email: user.email },
       process.env.SECRET_KEY,
       { expiresIn: "24h" }
     );
 
+    return { token, user: { id: user.id, email: user.email } };
+  }
 
-    return {token,user: {id: user.id,  email: user.email}};
-  };
+  async getUsers() {
+    const users = await User.findAll({});
+    return users;
+  }
+
+  async getUserById(id) {
+    const user = await User.findByPk(id);
+    return user;
+  }
+
+  // async updateUser (id, updateDate) {
+  //   const user = await User.findByPk(id);
+  //   if (updateDate.password) {
+  //     updateDate.password = await bcrypt.hash(updateDate.password, 10); 
+  //   };
+    
+  // }
 
 
 }
