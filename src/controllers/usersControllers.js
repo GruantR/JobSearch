7ac7ca4711d97822c6b2b9be17372e86f6104773
Ruslan_Trafimovich <rusworv@gmail.com>
2025-10-02 22:54:21 +0300

    gочти готовы к следующей таблице user_profile

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,6 +1,11 @@
-//usersControllers.js
+//src/controllers/usersControllers.js
 const usersServices = require("../services/usersServices");
-const { ValidationError, ForbiddenError, NotFoundError } = require("../errors/customErrors");
+const {
+  ValidationError,
+  ForbiddenError,
+  NotFoundError,
+  BadRequestError,
+} = require("../errors/customErrors");
 
 class UsersControllers {
   async createUser(req, res, next) {
@@ -27,6 +32,9 @@ class UsersControllers {
   async loginUser(req, res, next) {
     try {
       const { email, password } = req.body;
+      if (!email || !password) {
+        throw new ValidationError("Email и пароль обязательны");
+      }
       const tokenAndUser = await usersServices.authenticateUser(
         email,
         password
@@ -47,26 +55,25 @@ class UsersControllers {
       const users = await usersServices.getUsers();
       res.json({
         success: true,
+        message: "Список всех пользователей системы",
         data: { users },
       });
-    } catch (err) {
-
-    }
+    } catch (err) {}
   }
 
   async getCurrentUser(req, res, next) {
     try {
-
       if (req.params.id !== req.userId) {
-        throw new ForbiddenError('Нет доступа к данному ресурсу');
+        throw new ForbiddenError("Нет доступа к данному ресурсу");
       }
       const user = await usersServices.getUserById(req.userId);
 
       if (!user) {
-        throw new NotFoundError('Пользователь не найден');
+        throw new NotFoundError("Пользователь не найден");
       }
       res.json({
         success: true,
+        message: `Инофрмация пользователя ${req.user.email}`,
         data: { user },
       });
     } catch (err) {
@@ -76,24 +83,49 @@ class UsersControllers {
 
   async updateDataCurrentUser(req, res, next) {
     try {
-        if (req.params.id !== req.userId) {
-        throw new ForbiddenError('Нет доступа к данному ресурсу');
+      if (req.params.id !== req.userId) {
+        throw new ForbiddenError("Нет доступа к данному ресурсу");
       }
       const updateFields = {};
-      if (req.body.email) {updateFields.email = req.body.email};
-      if (req.body.password) {updateFields.password = req.body.password};
-
+      if (req.body) {
+        if (req.body.email) {
+          updateFields.email = req.body.email;
+        }
+        if (req.body.password) {
+          updateFields.password = req.body.password;
+        }
+      }
+      if (Object.keys(updateFields).length === 0) {
+        throw new BadRequestError("Нет данных для обновления");
+      }
       const user = await usersServices.patchDataUser(req.userId, updateFields);
-      
+
       res.json({
-        succes:true,
-        data: {user}
-      })
-  
+        success: true,
+        message: "Данные успешно обновлены",
+        data: { user },
+      });
     } catch (err) {
       next(err);
     }
+  }
 
+  async deleteCurrentUser(req, res, next) {
+    try {
+      if (req.params.id !== req.userId) {
+        throw new ForbiddenError(
+          `Нет доступа к данному ресурсу, Ваш id: ${req.userId}`
+        );
+      }
+      const deletedUser = await usersServices.dropUser(req.userId);
+      res.json({
+        success: true,
+        message: "Пользователь успешно удален из базы данных",
+        data: { deletedUser },
+      });
+    } catch (err) {
+      next(err);
+    }
   }
 }
 
