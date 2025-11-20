aa56a15ed8c15a05374b9d622a7a9b96b028b554
Ruslan_Trafimovich <rusworv@gmail.com>
2025-11-20 16:58:55 +0300

    done bot

--- src/bot/handlers/userHandlers.js
+++ src/bot/handlers/userHandlers.js
@@ -1,42 +1,42 @@
+//src/bot/handlers/userHandlers.js
 const sessionManager = require('../services/sessionManager');
 const UsersServices = require('../../services/usersService');
 const UserProfileService = require('../../services/userProfilesService');
 const { handleBotError } = require('../utils/errorHandler');
 const bot = require("../bot");
 
-class UserHandlers {
-  async handleMeAndProfileComand(msg) {
-    const chatId = msg.chat.id;
-    
-    try {
-      if (!sessionManager.isAuthenticated(chatId)) {
-        bot.sendMessage(chatId, '❌ Сначала войдите в систему через /login');
-        return;
-      }
+class userHandlers {
+    async handleMeAndProfileComand(msg) {
+        const chatId = msg.chat.id;
+        try {
+            if (!sessionManager.isAuthenticated(chatId)) {
+                bot.sendMessage(chatId, '❌ Сначала войдите в систему через /login');
+                return;
+            }
+            const session = sessionManager.getSession(chatId);
+            const userData = await UsersServices.getUserById(session.user.id);
+            const userProfile = await UserProfileService.getUserProfile(session.user.id)
+            const message = this.formatUserInfo(userData,userProfile)
+            
+            bot.sendMessage(chatId, message)
 
-      const session = sessionManager.getSession(chatId);
-      const userData = await UsersServices.getUserById(session.user.id);
-      const userProfile = await UserProfileService.getUserProfile(session.user.id);
-      
-      const message = this.formatUserInfo(userData, userProfile);
-      bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
 
-    } catch (error) {
-      const message = handleBotError(error);
-      bot.sendMessage(chatId, message);
+        } catch (error) {
+            const message = handleBotError(error);
+            bot.sendMessage(chatId, message);
+        }
     }
-  }
 
-  formatUserInfo(user, profile) {
-    return `
-👤 **Ваш профиль**
+    formatUserInfo (user, profile){
+        return `
+        👤 **Ваш профиль**
 
-📧 Email: ${user.email}
-👨‍💼 Имя: ${profile.fullName || 'Не указано'}
-📞 Телефон: ${profile.phoneNumber || 'Не указано'}
-📅 Зарегистрирован: ${user.createdAt.toLocaleDateString('ru-RU')}
-    `.trim();
-  }
+        📧 Email: ${user.email}
+        👨‍💼 Имя: ${profile.fullName || 'Не указано'}
+        📞 Телефон: ${profile.phoneNumber || 'Не указано'}
+        📅 Зарегистрирован: ${user.createdAt.toLocaleDateString('ru-RU')}
+        `.trim();
+    }
 }
 
-module.exports = new UserHandlers();
+module.exports = new userHandlers();
